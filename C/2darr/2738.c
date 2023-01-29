4ab80ba2defc645a4b1a02b324d4b20b21b1b0a7
HongMinSik <starwalk7058@naver.com>
2023-01-30 08:43:41 +0900

    Create 2738.c

---
+++ C/2darr/2738.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr1[n][m], arr2[n][m];
+
+    int i, j;
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            scanf("%d", &arr1[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            printf("%d ", arr1[i][j] + arr2[i][j]);
+        }
+        printf("\n");
+    }
+}
