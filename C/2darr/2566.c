d666da4ccc6b6126441c823a766f02695fd7e179
HongMinSik <starwalk7058@naver.com>
2023-01-30 08:42:49 +0900

    Create 2566.c

---
+++ C/2darr/2566.c
@@ -0,0 +1,28 @@
+```C
+#include<stdio.h>
+
+int main()
+{
+    int arr[9][9];
+    int i, j;
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int tmp = 0, a, b;
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            if(tmp <= arr[i][j]) {
+                tmp = arr[i][j];
+                a = i; b = j;
+            }
+        }
+    }
+
+    printf("%d\n", tmp);
+    printf("%d %d\n", a + 1, b + 1);
+
+}
+```
